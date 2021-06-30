6d4d6c67af1ef082469dffbb1fb87041b2450246
Trongduc0403 <trongduc0403@gmail.com>
2021-06-30 11:17:37 +0700

    Add project files.

---
+++ Controllers/CoursesController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using _1811062672_PhanNguyenTrongDuc_BigSchool.Models;
+using _1811062672_PhanNguyenTrongDuc_BigSchool.ViewModels;
+using Microsoft.AspNet.Identity;
+
+
+namespace _1811062672_PhanNguyenTrongDuc_BigSchool.Controllers
+{
+    public class CoursesController : Controller
+    {
+        private readonly ApplicationDbContext _dbContext;
+        public CoursesController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
+        // GET: Courses
+        [Authorize]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(CourseViewModel viewModel)
+        {
+            if(!ModelState.IsValid)
+            {
+                viewModel.Categories = _dbContext.Categories.ToList();
+                return View("Create", viewModel);
+            }    
+            var course = new Course
+            {
+                LecturerId = User.Identity.GetUserId(),
+                DateTime = viewModel.GetDateTime(),
+                CategoryId = viewModel.Category,
+                Place = viewModel.Place
+            };
+            _dbContext.Courses.Add(course);
+            _dbContext.SaveChanges();
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_1811062672_PhanNguyenTrongDuc_BigSchool.MvcApplication" Language="C#" %>
